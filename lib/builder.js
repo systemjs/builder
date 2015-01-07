systemjs/builder

commit b4ae08e7ad4770cfef2a4bca006248ce190fd399

api rejigging, minification support

lib/builder.js
@@ -3,6 +3,7 @@ var path = require('path');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 var asp = require('rsvp').denodeify;
+var uglify = require('uglify-js');
 
 function countLines(str) {
   return str.split(/\r\n|\r|\n/).length;
@@ -15,52 +16,115 @@ function countLines(str) {
 //                                   source map string] pairs
 //
 //  Takes lists as empty references and populates via push.
-function processOutputs(outputs, concatOutput, sourceMapsWithOffsets) {
+function processOutputs(outputs) {
   var offset = 0;
 
-  if (!outputs) return;
+  var outputObj = {};
+
+  var sources = outputObj.sources = [];
+  var sourceMapsWithOffsets = outputObj.sourceMapsWithOffsets = [];
+
+  if (!outputs)
+    return outputObj;
 
   outputs.forEach(function(output) {
     var source;
-    if (typeof output == "object") {
+    if (typeof output == 'object') {
       source = output.source || '';
       var offset_ = output.sourceMapOffset || 0;
       var map = output.sourceMap;
       if (map) {
         sourceMapsWithOffsets.push([offset + offset_, map]);
       }
-    } else if (typeof output == "string") {
+    } 
+    else if (typeof output == 'string') {
       source = output;
-    } else {
+    }
+    else {
       throw "Unexpected output format: " + output.toString();
     }
     source = saucy.removeSourceMaps(source || '');
     offset += countLines(source);
-    concatOutput.push(source);
+    sources.push(source);
   });
+
+  return outputObj;
 }
 
-exports.writeOutputFile = function(opts, outputs) {
-  var outFile = opts.outFile;
-  var createSourceMaps = opts.createSourceMaps;
-  var concatOutput = [];
-  var sourceMapsWithOffsets = [];
-
-  processOutputs(outputs, concatOutput, sourceMapsWithOffsets);
-
+function createOutput(outputs, outFile, createSourceMaps) {
   var outPath = path.dirname(outFile);
-  return asp(mkdirp)(outPath).then(function() {
-    if (createSourceMaps && (sourceMapsWithOffsets.length > 0)) {
-      var sourceOutFile = path.relative(outPath, outFile) + '.map';
-      var combinedSourceMap = saucy.concatenateSourceMaps(
-        outFile,
-        sourceMapsWithOffsets,
-        'file:' + path.resolve(outPath)
-        );
-      concatOutput.push('//# sourceMappingURL=' + sourceOutFile);
-      return asp(fs.writeFile)(sourceOutFile, combinedSourceMap);
-    }
-  }).then(function () {
-    return asp(fs.writeFile)(outFile, concatOutput.join('\n'));
+
+  // process output
+  var sourceMap;
+
+  var outputObj = processOutputs(outputs);
+
+  if (createSourceMaps && outputObj.sourceMapsWithOffsets.length)
+    sourceMap = saucy.concatenateSourceMaps(outFile, outputObj.sourceMapsWithOffsets, 
+        'file:' + path.resolve(outPath));
+  
+  var output = outputObj.sources.join('\n');
+
+  return {
+    source: output,
+    sourceMap: sourceMap
+  };
+}
+
+function minify(output, outFile) {
+  var fileName = path.basename(outFile);
+
+  var ast = uglify.parse(output.source, { filename: fileName });
+  ast.figure_out_scope();
+  ast = ast.transform(uglify.Compressor({ warnings: false }));
+  ast.figure_out_scope();
+  ast.compute_char_frequency();
+  ast.mangle_names({
+    except: ['require']
   });
+
+  var sourceMap;
+  if (output.sourceMap)
+    sourceMap = uglify.SourceMap({
+      file: fileName,
+      orig: output.sourceMap
+    });
+
+  output.source = ast.print_to_string({
+    // for some reason non-ascii broke esprima.... this does break unicode though
+    ascii_only: true,
+    // keep first comment
+    comments: function(node, comment) {
+      return comment.line == 1 && comment.col == 0;
+    },
+    source_map: sourceMap
+  });
+  output.sourceMap = sourceMap.toString();
+
+  return output;
+}
+
+
+exports.writeOutputFile = function(opts, outputs) {
+
+  var output = createOutput(outputs, opts.outFile, opts.sourceMaps);
+
+  if (opts.minify)
+    output = minify(output, opts.outFile);
+
+  if (opts.sourceMaps) {
+    var sourceMapFile = path.basename(opts.outFile) + '.map';
+    output.source += '//# sourceMappingURL=' + sourceMapFile;
+  }
+
+  return asp(mkdirp)(path.dirname(opts.outFile))
+  .then(function() {
+    if (!opts.sourceMaps)
+      return;
+    return asp(fs.writeFile)(sourceMapFile, output.sourceMap);
+  })
+  .then(function() {
+    return asp(fs.writeFile)(opts.outFile, output.source);
+  });
+
 };

compilers/amd.js
@@ -390,7 +390,7 @@ exports.remap = function(source, map, fileName) {
 exports.compile = function(load, opts, loader) {
   var normalize = opts.normalize;
   var options = {};
-  if (opts.createSourceMaps) {
+  if (opts.sourceMaps) {
     options.sourceMaps = 'memory';
   }
   var compiler = new traceur.Compiler(options);

compilers/cjs.js
@@ -69,7 +69,7 @@ exports.compile = function(load, opts, loader) {
     return source;
   })
   .then(function(source) {
-    if (!opts.createSourceMaps) {
+    if (!opts.sourceMaps) {
       return cjsOutput(load.name, deps, load.address, source, loader.baseURL);
     } else {
       var output = saucy.buildIdentitySourceMap(source, load.address);

compilers/es6.js
@@ -33,7 +33,7 @@ exports.compile = function(load, opts, loader) {
   options.modules = 'instantiate';
   options.script = false;
   options.moduleName = load.name;
-  if (opts.createSourceMaps) {
+  if (opts.sourceMaps) {
     options.sourceMaps = 'memory';
   }
 

compilers/global.js
@@ -29,7 +29,7 @@ exports.compile = function(load, opts, loader) {
                          load.metadata.deps;
 
 
-  if (!opts.createSourceMaps) {
+  if (!opts.sourceMaps) {
     return globalOutput(load.name, deps, load.metadata.exports, load.metadata.init, load.source);
   } else {
     var output = saucy.buildIdentitySourceMap(load.source, load.address);

index.js
@@ -42,14 +42,11 @@ exports.reset = reset;
 
 reset();
 
-exports.build = function(moduleName, config, outFile, createSourceMaps) {
-  if (arguments.length == 2) {
-    outFile = config;
-    config = null;
-  }
-  return exports.trace(moduleName, config)
+exports.build = function(moduleName, outFile, opts) {
+  opts = opts || {};
+  return exports.trace(moduleName, opts.config)
   .then(function(trace) {
-    return exports.buildTree(trace.tree, outFile, createSourceMaps);
+    return exports.buildTree(trace.tree, outFile, opts);
   });
 };
 
@@ -90,10 +87,12 @@ function compileLoad(load, opts, compilers) {
   });
 }
 
-exports.buildTree = function(tree, outFile, createSourceMaps) {
+exports.buildTree = function(tree, outFile, opts) {
+  opts = opts || {};
+  opts.outFile = outFile;
+
   var outputs = ['"format register";\n'];
   var names = Object.keys(tree);
-  var opts = {createSourceMaps: createSourceMaps, outFile: outFile};
 
   return Promise.all(names.map(function(name) {
     var load = tree[name];
@@ -103,15 +102,15 @@ exports.buildTree = function(tree, outFile, createSourceMaps) {
   .then(builder.writeOutputFile.bind(this, opts, outputs));
 };
 
-exports.buildSFX = function(moduleName, config, outFile, createSourceMaps) {
-  if (arguments.length == 2) {
-    outFile = config;
-    config = null;
-  }
+exports.buildSFX = function(moduleName, outFile, opts) {
+  opts = opts || {};
+  var config = opts.config;
+
+  opts.outFile = outFile;
 
   var outputs = [];
   var compilers = {};
-  var opts = {normalize: true, createSourceMaps: createSourceMaps, outFile: outFile};
+  opts.normalize = true;
   return exports.trace(moduleName, config)
   .then(function(trace) {
     var tree = trace.tree;

commit 6f95a6443d62dbdb880d5a31dc9edb6525e59512

Fixup null error
Looks like `--skip-source-map` was broken in https://github.com/systemjs/builder/commit/b4ae08e7ad4770cfef2a4bca006248ce190fd399

lib/builder.js
@@ -99,7 +99,7 @@ function minify(output, outFile) {
     },
     source_map: sourceMap
   });
-  output.sourceMap = sourceMap.toString();
+  output.sourceMap = sourceMap && sourceMap.toString();
 
   return output;
 }