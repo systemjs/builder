d397a4cce4ce3e3de23ad0b7c603aa9b96f29b2b
Chris Truter <jeffpalentine@gmail.com>
2015-02-10 12:33:34 +0200

    Set baseURL and update reference maps

--- test/unit-tests.js
+++ test/unit-tests.js
@@ -8,11 +8,16 @@ var err = function(e) {
   });
 };
 
+var buildOpts = {
+  sourceMaps: true,
+  config: {baseURL: 'test', map: {"jquery-cdn": "@empty"}}
+};
+
 var compareSourceMaps = function(filename, expectation, done) {
   builder.reset();
   builder.loadConfig('./test/cfg.js')
     .then(function() {
-      return builder.build(filename, null, { sourceMaps: true });
+      return builder.build(filename, null, buildOpts);
     })
   .then(function(output) {
     assert.equal(expectation, output.sourceMap.toString());
@@ -25,10 +30,7 @@ var buildOutput = function(filename) {
   builder.reset();
   builder.loadConfig('./test/cfg.js')
     .then(function() {
-      return builder.buildSFX(filename, './test/output.js', {
-        sourceMaps: true,
-        config: {baseURL: 'test', map: {"jquery-cdn": "@empty"}}
-      });
+      return builder.buildSFX(filename, './test/output.js', buildOpts);
     })
   .catch(err);
 };
@@ -47,11 +49,11 @@ describe('Source Maps', function() {
       var single = readExpectation('expectations/singleTraceur.json');
       var multiple = readExpectation('expectations/multipleTraceur.json');
       it('handles single compilation targets correctly', function(done) {
-        compareSourceMaps('test/tree/amd-2', single, done);
+        compareSourceMaps('tree/amd-2', single, done);
       });
 
       it('handles multiple compilation targets correctly', function(done) {
-        compareSourceMaps('test/tree/first', multiple, done);
+        compareSourceMaps('tree/first', multiple, done);
       });
     });
 
@@ -59,11 +61,11 @@ describe('Source Maps', function() {
       var single= readExpectation('expectations/singleChainTraceur.json');
       var multiple= readExpectation('expectations/multipleChainTraceur.json');
       it('handles single compilation targets correctly', function(done) {
-        compareSourceMaps('test/chain/second', single, done);
+        compareSourceMaps('chain/second', single, done);
       });
 
       it('handles multiple compilation targets correctly', function(done) {
-        compareSourceMaps('test/chain/first', multiple, done);
+        compareSourceMaps('chain/first', multiple, done);
       });
     });
   });
@@ -75,11 +77,11 @@ describe('Source Maps', function() {
       var single = readExpectation('expectations/single6to5.json');
       var multiple = readExpectation('expectations/multiple6to5.json');
       it('handles single compilation targets correctly', function(done) {
-        compareSourceMaps('test/tree/amd-2', single, done);
+        compareSourceMaps('tree/amd-2', single, done);
       });
 
       it('handles multiple compilation targets correctly', function(done) {
-        compareSourceMaps('test/tree/first', multiple, done);
+        compareSourceMaps('tree/first', multiple, done);
       });
     });
 
@@ -87,11 +89,11 @@ describe('Source Maps', function() {
       var single= readExpectation('expectations/singleChain6to5.json');
       var multiple= readExpectation('expectations/multipleChain6to5.json');
       it('handles single compilation targets correctly', function(done) {
-        compareSourceMaps('test/chain/second', single, done);
+        compareSourceMaps('chain/second', single, done);
       });
 
       it('handles multipl compilation targets correctly', function(done) {
-        compareSourceMaps('test/chain/first', multiple, done);
+        compareSourceMaps('chain/first', multiple, done);
       });
     });
   });
