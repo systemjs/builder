9f04964440c1f8d205b92aca56e94510a8a7e546
guybedford <guybedford@gmail.com>
2015-04-15 00:39:51 +0200

    arithmetic updates

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -5,44 +5,44 @@ builder.config({ transpiler: 'babel' });
 
 suite('Bundle Expressions', function() {
   test('Addition', function(done) {
-    builder.trace('amd + amd-2')
+    builder.trace('amd.js + amd-2.js')
     .then(function(tree) {
       assert.deepEqual(Object.keys(tree), 
-          ['amd-2', 'jquery', 'some!plugin', 'text.txt!text-plugin', 'global', 'amd']);
+          ['amd-2.js', 'jquery.js', 'some.js!plugin.js', 'text.txt!text-plugin.js', 'global.js', 'amd.js']);
     })
     .then(done, done);
   });
 
   test('Single module subtraction', function(done) {
-    builder.trace('amd + amd-2 - [amd-1]')
+    builder.trace('amd.js + amd-2.js - [amd-1.js]')
     .then(function(tree) {
       assert.deepEqual(Object.keys(tree), 
-          ['amd-2', 'jquery', 'some!plugin', 'text.txt!text-plugin', 'global', 'amd']);
+          ['amd-2.js', 'jquery.js', 'some.js!plugin.js', 'text.txt!text-plugin.js', 'global.js', 'amd.js']);
     })
     .then(done, done);
   });
 
   test('Commonality operator', function(done) {
-    builder.trace('amd-5b & second')
+    builder.trace('amd-5b.js & second.js')
     .then(function(tree) {
-      assert.deepEqual(Object.keys(tree), ['third', 'cjs', 'second']);
+      assert.deepEqual(Object.keys(tree), ['third.js', 'cjs.js', 'second.js']);
     })
     .then(done, done);
   });
 
   test('Wildcard bundling', function(done) {
-    builder.trace('* - [amd-*]')
+    builder.trace('*.js - [amd-*]')
     .then(function(tree) {
       assert.deepEqual(Object.keys(tree), [
-          'cjs', 'umd', 'second', 'third', 'text-plugin', 'plugin', 'babel',
-          'jsx', 'jquery', 'global', 'global-outer', 'global-inner', 'some!plugin', 
-          'text.txt!text-plugin', 'component.jsx!jsx', 'amd', 'first']);
+          'cjs.js', 'umd.js', 'second.js', 'third.js', 'text-plugin.js', 'plugin.js', 'babel',
+          'jsx.js', 'jquery.js', 'global.js', 'global-outer.js', 'global-inner.js', 'some.js!plugin.js', 
+          'text.txt!text-plugin.js', 'component.jsx!jsx.js', 'amd.js', 'first.js']);
     })
     .then(done, done);
   });
 
   test('Wildcard plugin', function(done) {
-    builder.trace('*.jsx! - [component.jsx!]')
+    builder.trace('*.jsx!jsx.js - [component.jsx!jsx.js]')
     .then(function(tree) {
       assert.deepEqual(Object.keys(tree), []);
     })
