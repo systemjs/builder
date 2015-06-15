f6397b68f987bb5eface22b44131c003f8be2f6f
guybedford <guybedford@gmail.com>
2015-06-15 17:32:27 +0200

    all tests automated

--- test/bundle-format.js
+++ test/bundle-format.js
@@ -5,12 +5,14 @@ var spawn = require('child_process').spawn;
 suite('Bundle Format', function() {
   test('AMD', function(done) {
     builder.buildSFX('sfx-format-01.js', 'test/output/sfx-amd.js', { sfxFormat: 'amd' })
-      .then(function () {
-        var child = spawn('node_modules/.bin/mocha-phantomjs', ['test/test-sfx-amd.html'])
-          .on('close', function (code) {
-            assert.deepEqual(code, 0);
-            done();
-          });
-      }, done);
+    .then(function () {
+      spawn('node_modules/.bin/mocha-phantomjs', ['test/test-sfx-amd.html'])
+      .on('close', function (code) {
+        assert.deepEqual(code, 0);
+        done();
+      })
+      .on('error', done);
+    })
+    .catch(done);
   });
 });
--- test/test-build.js
+++ test/test-build.js
@@ -1,6 +1,7 @@
 var Builder = require('../index');
 var inline = require('../lib/output').inlineSourceMap;
 var fs = require('fs');
+var spawn = require('child_process').spawn;
 if (process.argv[2] == 'typescript')
   global.ts = require('typescript');
 
@@ -12,15 +13,15 @@ var err = function(e) {
   });
 };
 
-var builder = new Builder('fixtures/test-tree');
+var builder = new Builder('test/fixtures/test-tree');
+
 var cfg = {
-  transpiler: process.argv[2] == 'babel' || process.argv[2] == 'typescript' ? process.argv[2] : 'traceur',
   paths: {
     'jquery-cdn': 'https://code.jquery.com/jquery-2.1.1.min.js',
-    'babel': '../node_modules/babel-core/browser.js',
-    'babel/external-helpers': '../node_modules/babel-core/external-helpers.js',
-    'traceur': '../node_modules/traceur/bin/traceur.js',
-    'traceur-runtime': '../node_modules/traceur/bin/traceur-runtime.js'
+    'babel': './node_modules/babel-core/browser.js',
+    'babel/external-helpers': './node_modules/babel-core/external-helpers.js',
+    'traceur': './node_modules/traceur/bin/traceur.js',
+    'traceur-runtime': './node_modules/traceur/bin/traceur-runtime.js'
   },
   meta: {
     'jquery-cdn': {
@@ -29,115 +30,153 @@ var cfg = {
   }
 };
 
-console.log('Running in-memory build...');
-builder.config(cfg);
-
-builder.build('first.js', null, { sourceMaps: true, minify: minify })
-.then(function(output) {
-  fs.writeFile('output/memory-test.js', inline(output));
-  console.log('Wrote in-memory build to ./memory-test');
-})
-.catch(err);
-
-console.log('Running a multi-format build...');
-
-builder.config(cfg);
-
-builder.build('first.js', 'output/tree-build.js', { sourceMaps: true, minify: minify, globalDefs: { DEBUG: false } })
-.then(function() {
-  console.log('Done');
-})
-.catch(err);
-
-var treeFirst;
-Promise.all(['first.js', 'amd.js'].map(builder.trace.bind(builder)))
-.then(function(trees) {
-  treeFirst = trees[0];
-  return builder.buildTree(builder.subtractTrees(trees[0], trees[1]), 'output/excluded.js');
-})
-
-.then(function() {
-  return builder.trace('global-inner.js').then(function(tree) {
-    return builder.buildTree(tree, 'output/global-inner.js');
+function testPhantom(html, done) {
+  spawn('node_modules/.bin/mocha-phantomjs', [html], { stdio: 'inherit' })
+  .on('close', function(code) {
+    if (code !== 0)
+      throw new Error('Phantom test failed');
+    done();
   });
-})
+}
 
-.then(function() {
-  return builder.trace('global-outer.js').then(function(tree) {
-    return builder.buildTree(tree, 'output/global-outer.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-1.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-1.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-2.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-2.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-3.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-3.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-4.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-4.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-5a.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-5a.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-5b.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-5b.js');
-  });
-})
-
-
-.then(function() {
-  return builder.trace('amd-6a.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-6a.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('amd-6b.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/amd-6b.js');
-  });
-})
-
-.then(function() {
-  return builder.trace('umd.js').then(function(tree) {
-    return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'output/umd.js');
-  });
-})
-
-.then(function() {
-  return builder.build('amd-7.js', 'output/amd-7.js');
-})
-
-.then(function() {
+function doTests(transpiler) {
   builder.reset();
   builder.config(cfg);
-  builder.config({ 
-    map: { 
-    'jquery-cdn': '@empty',
-    'toamd1': 'amd-1.js'
-    }
+  builder.config({ transpiler: transpiler });
+  
+  test('In-memory build', function(done) {
+    builder.build('first.js', null, { sourceMaps: true, minify: minify })
+    .then(function(output) {
+      fs.writeFileSync('test/output/memory-test.js', inline(output));
+      done();
+    })
+    .catch(done);
   });
-  return builder.buildSFX('toamd1', 'output/sfx.js', { runtime: true, minify: minify, globalDefs: { DEBUG: false } });
-})
-.catch(err);
 
+  test('Multi-format tree build', function(done) {
+
+    builder.build('first.js', 'test/output/tree-build.js', { sourceMaps: true, minify: minify, globalDefs: { DEBUG: false } })
+    .then(function() {
+      var treeFirst;
+      Promise.all(['first.js', 'amd.js'].map(builder.trace.bind(builder)))
+      .then(function(trees) {
+        treeFirst = trees[0];
+        return builder.buildTree(builder.subtractTrees(trees[0], trees[1]), 'test/output/excluded.js');
+      })
+
+      .then(function() {
+        return builder.trace('global-inner.js').then(function(tree) {
+          return builder.buildTree(tree, 'test/output/global-inner.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('global-outer.js').then(function(tree) {
+          return builder.buildTree(tree, 'test/output/global-outer.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-1.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-1.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-2.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-2.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-3.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-3.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-4.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-4.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-5a.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-5a.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-5b.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-5b.js');
+        });
+      })
+
+
+      .then(function() {
+        return builder.trace('amd-6a.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-6a.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('amd-6b.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/amd-6b.js');
+        });
+      })
+
+      .then(function() {
+        return builder.trace('umd.js').then(function(tree) {
+          return builder.buildTree(builder.subtractTrees(tree, treeFirst), 'test/output/umd.js');
+        });
+      })
+
+      .then(function() {
+        return builder.build('amd-7.js', 'test/output/amd-7.js');
+      })
+    })
+    .then(function () {
+      testPhantom('test/test-build.html', done);
+    })
+    .catch(done);
+  });
+
+  // traceur runtime function.bind fails in Phantom
+  if (transpiler != 'traceur')
+  test('SFX tree build', function(done) {
+    builder.reset();
+    builder.config(cfg);
+    builder.config({transpiler: transpiler });
+    builder.config({ 
+      map: { 
+      'jquery-cdn': '@empty',
+      'toamd1': 'amd-1.js'
+      }
+    });
+    return builder.buildSFX('toamd1', 'test/output/sfx.js', { runtime: true, minify: minify, globalDefs: { DEBUG: false } })
+    .then(function() {
+      testPhantom('test/test-sfx.html', done);
+    })
+    .catch(done);
+  });
+}
+
+suite('Test tree builds - Traceur', function() {
+
+  doTests('traceur');
+
+});
+
+suite('Test tree builds - Babel', function() {
+
+  doTests('babel');
+
+});
+
+
+/* suite('Test tree builds - TypeScript', function() {
+  
+  doTests('typescript');
+
+}); */
 
