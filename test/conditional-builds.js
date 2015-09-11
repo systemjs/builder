d76ef6c17c00e246f37f3d85f3ab46c4680121ca
guybedford <guybedford@gmail.com>
2015-09-11 11:46:13 +0200

    conditional tracing support

---
+++ test/conditional-builds.js
@@ -0,0 +1,23 @@
+var Builder = require('../index');
+
+var builder = new Builder('test/fixtures/conditional-tree');
+
+builder.loadConfigSync('test/fixtures/conditional-tree.config.js');
+
+suite('Conditional Builds', function() {  
+  test('Package environment traces all conditional variations', function() {
+    return builder.trace('pkg/env-condition')
+    .then(function(trace) {
+      assert.deepEqual(Object.keys(trace).sort(), ['pkg#:env-condition', '@system-env', 'pkg/env-condition-browser.js', 'pkg/env-condition.js'].sort());
+    });
+  });
+
+  // return builder.trace('pkg/env-condition!plugin')
+
+  test('Conditional interpolation traces all conditional variations', function() {
+    return builder.trace('interpolated-#{conditions.js|test}.js')
+    .then(function(trace) {
+      assert.deepEqual(Object.keys(trace).sort(), [ 'interpolated-#{conditions.js|test}.js', 'conditions.js', 'interpolated-1.js', 'interpolated-2.js' ].sort());
+    });
+  });
+});
