7643b2f53d489736257686b1bfa3399ea7b0facf
guybedford <guybedford@gmail.com>
2015-11-15 00:16:34 +0200

    new algorithm approach, work in progress

--- test/static-optimize.js
+++ test/static-optimize.js
@@ -14,10 +14,9 @@ builder.config({
 
 suite('SFX Optimizations', function() {
   test('All ES6 rollup optimization', function(done) {
-    builder.buildSFX('a.js', 'test/output/es-sfx.js', { runtime: false, minify: minify })
+    builder.buildStatic('a.js', 'test/output/es-sfx.js', { runtime: false, minify: minify })
     .then(function(output) {
       // TODO actually test
-      console.log(output);
       done();
     }, done)
   });
