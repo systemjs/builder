34033865f83eb49ede7ae5c672903ee9a1de37cf
guybedford <guybedford@gmail.com>
2015-11-23 14:00:07 +0200

    revert back to post order over reverse pre order

--- test/test-post-order.js
+++ test/test-post-order.js
@@ -1,5 +1,5 @@
 var expect = require('unexpected');
-var getTreeModulesPostOrder = require('../lib/compile').getTreeModulesReversePreOrder;
+var getTreeModulesPostOrder = require('../lib/compile').getTreeModulesPostOrder;
 
 suite('Test post order traversal', function() {
   test('should return single module that has no incoming relation', function() {
@@ -28,7 +28,7 @@ suite('Test post order traversal', function() {
       }
     };
 
-    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['b', 'a']);
+    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['a', 'b']);
   });
 
   test('should resolve module names based on depMap', function() {
@@ -79,7 +79,7 @@ suite('Test post order traversal', function() {
       }
     };
 
-    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['d', 'c', 'b', 'a']);
+    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['c', 'b', 'd', 'a']);
   });
 
   test('should order graph entries alphabetically', function() {
@@ -110,38 +110,7 @@ suite('Test post order traversal', function() {
       }
     };
 
-    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['d', 'c', 'b', 'a']);
-  });
-
-  test('should override alphabetical graph entry order with entryPoints array', function() {
-    var tree = {
-      'a': {
-        name: 'a',
-        deps: ['b'],
-        depMap: {
-          'b': 'b'
-        }
-      },
-      'b': {
-        name: 'b',
-        deps: ['c'],
-        depMap: {
-          'c': 'c'
-        }
-      },
-      'c': {
-        name: 'c',
-        deps: [],
-        depMap: {}
-      },
-      'd': {
-        name: 'd',
-        deps: [],
-        depMap: {}
-      }
-    };
-
-    return expect(getTreeModulesPostOrder(tree, ['d', 'a']).modules, 'to satisfy', ['d', 'c', 'b', 'a']);
+    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['c', 'b', 'a', 'd']);
   });
 
   test('should include entry points not present in given entryPoints order, in alphabetical order', function() {
@@ -177,6 +146,6 @@ suite('Test post order traversal', function() {
       }
     };
 
-    return expect(getTreeModulesPostOrder(tree, ['d']).modules, 'to satisfy', ['e', 'd', 'c', 'b', 'a']);
+    return expect(getTreeModulesPostOrder(tree).modules, 'to satisfy', ['c', 'b', 'a', 'd', 'e']);
   });
 });
