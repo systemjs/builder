f95fde4e4e9edbf349c76904230f8d83a6438263
guybedford <guybedford@gmail.com>
2015-12-17 14:51:49 +0200

    allow compileOpts.entryPoints, expand AMD template to be UMD

--- templates/sfx-amd.js
+++ templates/sfx-amd.js
@@ -1,6 +1,11 @@
 (function(factory) {
+  ${ deps.length ? 'var deps = ' + JSON.stringify(deps) + ';\n' : '' }
   if (typeof define == 'function' && define.amd)
-    define(${JSON.stringify(deps)}, factory);
+    define(${ deps.length ? 'deps' : '[]' }, factory);
+  else if (typeof module == 'object' && module.exports && typeof require == 'function')
+    module.exports = factory${ deps.length ? '.apply(null, deps.map(require))' : '()' };
   else
-    factory();
+    ${ deps.length 
+      ? 'throw new Error("Module must be loaded as AMD or CommonJS")'
+      : 'factory()'};
 });
