56eab82cbe894932a1ce2b323f27a2cb2c804847
guybedford <guybedford@gmail.com>
2015-12-19 14:17:32 +0200

    separate amd from umd

---
+++ templates/sfx-umd.js
@@ -0,0 +1,11 @@
+(function(factory) {
+  ${ deps.length ? 'var deps = ' + JSON.stringify(deps) + ';\n' : '' }
+  if (typeof define == 'function' && define.amd)
+    define(${ deps.length ? 'deps' : '[]' }, factory);
+  else if (typeof module == 'object' && module.exports && typeof require == 'function')
+    module.exports = factory${ deps.length ? '.apply(null, deps.map(require))' : '()' };
+  else
+    ${ deps.length 
+      ? 'throw new Error("Module must be loaded as AMD or CommonJS")'
+      : 'factory()'};
+});
