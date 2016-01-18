085e1b681c8921b904173437344d2e763821eb95
guybedford <guybedford@gmail.com>
2016-01-18 22:02:27 +0200

    update umd template

--- templates/sfx-umd.js
+++ templates/sfx-umd.js
@@ -1,9 +1,10 @@
 (function(factory) {
-  ${ deps.length ? 'var deps = ' + JSON.stringify(deps) + ';\n' : '' }
   if (typeof define == 'function' && define.amd)
-    define(${ deps.length ? 'deps' : '[]' }, factory);
+    define(${JSON.stringify(deps)}, factory);
   else if (typeof module == 'object' && module.exports && typeof require == 'function')
-    module.exports = factory${ deps.length ? '.apply(null, deps.map(require))' : '()' };
+    module.exports = factory(${deps.map(function(dep) {
+    return 'require("' + dep + '")';
+  }).join(', ')});
   else
     ${ deps.length && !globalDeps.length
       ? 'throw new Error("Module must be loaded as AMD or CommonJS")'
