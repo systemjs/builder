419c20c4411bca7e50aa2c45f6024a0680a5c6ad
guybedford <guybedford@gmail.com>
2016-02-16 01:21:35 +0200

    ensure external depMap merging through rollup optimization (#476)

--- test/static-optimize.js
+++ test/static-optimize.js
@@ -29,7 +29,7 @@ builder.config({
 
 suite('SFX Optimizations', function() {
   test('All ES6 rollup optimization', function(done) {
-    builder.buildStatic('a.js', 'test/output/es-sfx.js', { runtime: false, minify: minify, format: 'esm' })
+    builder.buildStatic('a.js', 'test/output/es-sfx.js', { runtime: false, minify: minify, format: 'esm', rollup: true })
     .then(function(output) {
       assert(output.source, 'var b = \'b\';\n\nvar a = \'a\';\n\nexport { a, b };');
       done();
@@ -37,9 +37,9 @@ suite('SFX Optimizations', function() {
   });
 
   test('ES6 rollup with a global dep', function(done) {
-    builder.buildStatic('global-dep.js', 'test/output/es-sfx.js', { runtime: false, minify: minify, format: 'esm', encodeNames: true })
+    builder.buildStatic('global-dep.js', 'test/output/es-sfx.js', { runtime: false, minify: minify, format: 'esm', rollup: true })
     .then(function(output) {
-      assert(output.source.indexOf('System.registerDynamic("4"') != -1 && output.source.indexOf(', ["4"') != -1);
+      assert(output.source.indexOf('System.registerDynamic("2"') != -1 && output.source.indexOf(', ["2"') != -1);
       done();
     }, done);
   });
