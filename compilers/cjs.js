3639af4373c26b0e017975a4c503f257a8143428
guybedford <guybedford@gmail.com>
2016-02-25 16:43:48 +0200

    correction for commonjs global

--- compilers/cjs.js
+++ compilers/cjs.js
@@ -145,7 +145,7 @@ CJSRegisterTransformer.prototype.transformScript = function(tree) {
   var useStrict = hasRemoveUseStrict(scriptItemList) && [createUseStrictDirective()] || [];
 
   scriptItemList = useStrict.concat(parseStatements([
-    globalExpression + nl + 'var define;'
+    globalExpression + nl + 'var define;' + nl + 'var global = this;' + nl + 'var GLOBAL = this;'
   ])).concat(scriptItemList).concat(parseStatements([
     'return module.exports;'
   ]));
