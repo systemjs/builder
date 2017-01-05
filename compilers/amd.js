0c6aaa6203c54eef127ce6d2225a6debd4400d4a
jrauschenbusch <jochen.rauschenbusch@gmail.com>
2017-01-05 18:36:45 +0100

    Update AMD system wrapper to v0.1.0

--- compilers/amd.js
+++ compilers/amd.js
@@ -1,171 +1,6 @@
-var compiler = require('./compiler');
-
-var traceur = require('traceur');
 var vm = require('vm');
-var traceurGet = require('../lib/utils').traceurGet;
-
-var ParseTreeTransformer = traceurGet('codegeneration/ParseTreeTransformer.js').ParseTreeTransformer;
-var parseExpression = traceurGet('codegeneration/PlaceholderParser.js').parseExpression;
-
-var CJSRequireTransformer = require('./cjs').CJSRequireTransformer;
+var compiler = require('./compiler');
 var Promise = require('bluebird');
-
-// First of two-pass transform
-// lists number of define statements, the named module it defines (if any), and deps
-// second pass will do rewriting based on this info
-// we set this.anonDefine, which is true if there is one named define, or one anonymous define
-// if there are more than one anonymous defines, it is invalid
-function AMDDependenciesTransformer(map) {
-  // optional mapping function
-  this.map = map;
-  this.anonDefine = false;
-  this.anonDefineIndex = -1;
-  this.anonNamed = false;
-  this.deps = [];
-  this.bundleDefines = [];
-  this.defineRedefined = false;
-  return ParseTreeTransformer.call(this);
-}
-AMDDependenciesTransformer.prototype = Object.create(ParseTreeTransformer.prototype);
-AMDDependenciesTransformer.prototype.filterAMDDeps = function (deps) {
-  var newDeps = [];
-  var bundleDefines = this.bundleDefines;
-  deps.forEach(function (dep) {
-    if (['require', 'exports', 'module'].indexOf(dep) != -1)
-      return;
-    if (bundleDefines.indexOf(dep) != -1)
-      return;
-    newDeps.push(dep);
-  });
-  return newDeps;
-};
-// var define = x is the stopping point for handling a define
-// we still allow (function(define) {})
-// these are the same rules of the r.js optimizer
-
-// var define disables until we quit the existing scope
-AMDDependenciesTransformer.prototype.transformVariableDeclaration = function (tree) {
-  if (tree.lvalue.identifierToken && tree.lvalue.identifierToken.value == 'define')
-    this.defineRedefined = true;
-  return tree;
-};
-// this catches the scope exit, although should be better handled than this (eg blocks for ES6)
-AMDDependenciesTransformer.prototype.transformFunctionDeclaration = function (tree) {
-  var defineRedefined = this.defineRedefined;
-  tree = ParseTreeTransformer.prototype.transformFunctionDeclaration.call(this, tree);
-  if (defineRedefined === false)
-    this.defineRedefined = false;
-  return tree;
-};
-AMDDependenciesTransformer.prototype.transformFunctionExpression = function (tree) {
-  var defineRedefined = this.defineRedefined;
-  tree = ParseTreeTransformer.prototype.transformFunctionExpression.call(this, tree);
-  if (defineRedefined === false)
-    this.defineRedefined = false;
-  return tree;
-};
-AMDDependenciesTransformer.prototype.transformCallExpression = function (tree) {
-  if (this.defineRedefined || !tree.operand.identifierToken || tree.operand.identifierToken.value != 'define')
-    return ParseTreeTransformer.prototype.transformCallExpression.call(this, tree);
-
-  var args = tree.args.args;
-  var name;
-  var depArg = -1;
-  if (args[0].type == 'LITERAL_EXPRESSION' || args[1] && args[1].type == 'ARRAY_LITERAL') {
-    name = args[0].literalToken && args[0].literalToken.processedValue || true;
-    if (args[1] && args[1].type == 'ARRAY_LITERAL')
-      depArg = 1;
-  }
-  else if (args[0].type == 'ARRAY_LITERAL') {
-    depArg = 0;
-  }
-
-  var factoryArg = name && depArg == -1 ? 1 : depArg + 1;
-
-  // ignore requires of the wrong form
-  if (!args[factoryArg])
-    return ParseTreeTransformer.prototype.transformCallExpression.call(this, tree);
-
-  // note the define index
-  // so we know which one to name for the second pass
-  if (!this.anonDefine || this.anonNamed)
-    this.anonDefineIndex++;
-
-  var parseDeps = false;
-
-  // anonymous define
-  if (!name) {
-    if (this.anonDefine && !this.anonNamed)
-      throw new Error('Multiple anonymous defines.');
-
-    this.anonDefine = true;
-    this.anonNamed = false;
-    parseDeps = true;
-  }
-  // named define
-  else {
-    if (typeof name != 'boolean') {
-      this.bundleDefines.push(name);
-      // remove any deps which exactly reference a name
-      var depsIndex = this.deps.indexOf(name);
-      if (depsIndex != -1)
-        this.deps.splice(depsIndex, 1);
-    }
-    if (!this.anonDefine && this.anonDefineIndex == 0 && typeof name != 'boolean') {
-      this.anonDefine = true;
-      this.anonNamed = true;
-      parseDeps = true;
-    }
-    else if (this.anonDefine && this.anonNamed) {
-      this.anonDefine = false;
-      this.anonNamed = false;
-      this.deps = [];
-    }
-  }
-
-  // only continue to extracting dependencies if this is THE anonymous define
-  if (!parseDeps)
-    return ParseTreeTransformer.prototype.transformCallExpression.call(this, tree);
-
-  if (depArg != -1) {
-    var deps = args[depArg].elements.map(function (dep) {
-      return dep.literalToken.processedValue;
-    });
-
-    // apply the map
-    var depMap = this.map;
-    if (depMap)
-      deps = deps.map(function (dep) {
-        if (['require', 'exports', 'module'].indexOf(dep) != -1)
-          return dep;
-        return depMap(dep);
-      });
-
-    // store dependencies for trace
-    this.deps = this.filterAMDDeps(deps);
-
-    // this is ONLY a mutation for remap which will be deprecated
-    args[depArg] = parseExpression([JSON.stringify(deps)]);
-
-    return ParseTreeTransformer.prototype.transformCallExpression.call(this, tree);
-  }
-
-  if (depArg == -1 && args[factoryArg].type == 'FUNCTION_EXPRESSION') {
-    var cjsFactory = args[factoryArg];
-    // now we need to do a scope transformer for the require function at this position
-    var fnParameters = cjsFactory.parameterList.parameters;
-    var reqName = fnParameters[0] && fnParameters[0].parameter.binding.identifierToken.value;
-
-    // now we create a new scope transformer and apply it to this function to find every call of
-    // the function reqName, noting the require
-    var cjsRequires = new CJSRequireTransformer(reqName);
-    cjsFactory.body = cjsRequires.transformAny(cjsFactory.body);
-    this.deps = this.filterAMDDeps(cjsRequires.requires);
-  }
-
-  return ParseTreeTransformer.prototype.transformCallExpression.call(this, tree);
-};
-exports.AMDDependenciesTransformer = AMDDependenciesTransformer;
 
 // override System instantiate to handle AMD dependencies
 exports.attach = function (loader) {
@@ -178,10 +13,11 @@ exports.attach = function (loader) {
 	  
         if (!load.source) load.source = ' ';
 
-        var output = compiler.transformAMDDependencies(load);
+        var output = compiler.compileAst(load, [require('babel-plugin-transform-amd-system-wrapper').default, { filterMode: true }]);
+        load.metadata.ast = output.ast;
 
         var entry = loader.defined[load.name];
-        entry.deps = dedupe(output.amdDeps.concat(load.metadata.deps));
+        entry.deps = dedupe(output.metadata.amdDeps.concat(load.metadata.deps));
 
         load.metadata.builderExecute = function (require, exports, module) {
           var removeDefine = loader.get('@@amd-helpers').createDefine(loader);
@@ -249,28 +85,15 @@ function group(deps) {
   return { names: names, indices: indices };
 }
 
-
-exports.remap = function (source, map, fileName) {
-  var options = { script: true, sourceRoot: true };
-  var compiler = new traceur.Compiler(options);
-  var tree = compiler.parse(source, fileName || '');
-  var transformer = new AMDDependenciesTransformer(map);
-  tree = transformer.transformAny(tree);
-
-  var output = compiler.write(tree);
-  return Promise.resolve(output);
-};
-
-
 // converts anonymous AMDs into named AMD for the module
 exports.compile = function (load, opts, loader) {
-  opts.moduleId = !opts.anonymous && load.name; 
+  opts.moduleId = !opts.anonymous && load.name;
   return compiler.compile(load, opts, [require('babel-plugin-transform-amd-system-wrapper').default, {
     map: function (dep) {
       return opts.normalize ? load.depMap[dep] : dep;
     },
     systemGlobal: opts.systemGlobal,
-	deps: load.deps
+	  deps: load.deps
   }]);
 };
 
--- compilers/compiler.js
+++ compilers/compiler.js
@@ -6,43 +6,47 @@ function pathToUrl(p) {
 }
 
 exports.compile = function (load, opts, plugin) {
-  var sourceRoot = pathToUrl(path.dirname(load.path) + '/');
+  var sourceRoot = load.path && pathToUrl(path.dirname(load.path) + '/');
   var options = {
     babelrc: false,
     compact: false,
     sourceType: 'script',
-    filename: pathToUrl(load.path),
-    filenameRelative: path.basename(load.path),
-    inputSourceMap: load.metadata.sourceMap,
+    filename: load.path && pathToUrl(load.path),
+    filenameRelative: load.path && path.basename(load.path),
+    inputSourceMap: load.metadata && load.metadata.sourceMap,
     moduleId: opts.moduleId,
-    sourceFileName: path.basename(load.path),
+    sourceFileName: load.path && path.basename(load.path),
     sourceMaps: opts.sourceMaps,
     sourceRoot: sourceRoot,
     plugins: [plugin]
   };
+  
+  var source = load.metadata.originalSource || load.source;
 
-  var output = babel.transform(load.source, options);
+  var output;
+  if (load.metadata.ast) {
+    output = babel.transformFromAst(load.metadata.ast, source, options);
+  } else {
+    output = babel.transform(source, options);
+  }
 
   var sourceMap = output.map;
   if (sourceMap && !sourceMap.sourceRoot) // if input source map doesn't have sourceRoot - add it
     sourceMap.sourceRoot = sourceRoot;
-
+  
   return Promise.resolve({
     source: output.code,
     sourceMap: sourceMap
   });
 };
 
-exports.transformAMDDependencies = function (load) {
-  var options = {
-    plugins: [[require('babel-plugin-transform-amd-system-wrapper').default, {
-      filterMode: true
-    }]]
-  };
-
-  var output = babel.transform(load.source, options);
-
-  return {
-    amdDeps: output.metadata.amdDeps
-  };
+exports.compileAst = function (load, plugin) {
+  return babel.transform(load.source, {
+      babelrc: false,
+      compact: false,
+      filename: load.path,
+      inputSourceMap: load.metadata.sourceMap,
+      ast: true,
+      plugins: [plugin]
+    });
 }
